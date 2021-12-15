eff76af1afcf1c02d6a34adb640cdad328f77ddb
harrymueller <harry@mueller.com.au>
2021-12-15 03:28:45 +0000

    exec.sh

--- docker/exec.sh
+++ docker/exec.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
-nohup docker run -v /data/tongue:/mnt/data tonguediscovery bash $1 >> log.out &
+# run bash script ($1) in disconnected docker container
+# $1 relative to top dir of git repo
+nohup docker run -rm -v /data/tongue:/mnt/data tonguediscovery bash /mnt/data/scripts/$1 >> log.out &
