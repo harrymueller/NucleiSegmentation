f9f65b6b76690413ab266975699f49d2eab2b9a8
harrymueller <58456893+harrymueller@users.noreply.github.com>
2022-01-11 16:08:03 +0800

    Delete temp.sh

--- temp.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-for id in "tongue-4" "tongue-5"; do
-	echo $id
-	Rscript rscripts/cli/spatial_plotting.R --binsize 10 --id $id --diameter 60
-	Rscript rscripts/cli/spatial_plotting.R --binsize 20 --id $id --diameter 40
-	Rscript rscripts/cli/spatial_plotting.R --binsize 30 --id $id --diameter 25
-done
