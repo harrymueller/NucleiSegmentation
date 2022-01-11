7083029da03474de0ef38b5a46a1c4d6a7cfcf2c
Harry <harry@mueller.com.au>
2022-01-11 10:55:04 +0800

    fixed some cli scripts

--- rscripts/cli/dim_reduction.R
+++ rscripts/cli/dim_reduction.R
@@ -2,20 +2,32 @@ options(future.globals.maxSize = 100000000 * 1024^2,scipen = 20000000)
 set.seed(6)
 
 library(argparser)
+library(Seurat)
+
 args <- arg_parser("Loading BGI spatial data(.gem)")
 args <- add_argument(args, "--binsize", help = "binsize")
 args <- add_argument(args, "--id", help="id")
+args <- add_argument(args, "--method", help="method of normalisation, SCT || LN")
 argv <- parse_args(args)
 
+TONGUE_ID = argv$id
+BIN_SIZE = argv$binsize
+METHOD = argv$method
 
-OUTPUT = sprintf("/data/tongue/dimReducedRDS/%s_bin%s_red.Rds", TONGUE_ID, BIN_SIZE)
-
-DIR = "/mnt/data/tongue_STOmics/discovery/gemRDS"
+DIR = "/mnt/data/gemRDS"
 INPUT = sprintf("%s/%s_bin%s_spatialObj.rds", DIR, TONGUE_ID, BIN_SIZE)
-obj = readRDS(INPUT)
+obj = readRDS(INPUT)    
 
-# method of normalisation
-obj <- SCTransform(obj, assay = "Spatial", verbose = FALSE)
+if (METHOD == "SCT") {
+    OUTPUT = sprintf("/mnt/data/scDimReducedRDS/%s_bin%s_red.Rds", TONGUE_ID, BIN_SIZE)
+    obj <- SCTransform(obj, assay = "Spatial", verbose = FALSE)
+} else if (METHOD == "LN") {
+    OUTPUT = sprintf("/mnt/data/dimReducedRDS/%s_bin%s_red.Rds", TONGUE_ID, BIN_SIZE)
+    obj <- NormalizeData(obj, assay = "Spatial", verbose = FALSE)
+} else {
+    sprintf("Unrecognised parameter for --method: %s. Use SCT or LN", METHOD)
+    exit()
+}
 
 # dimension reduction
 obj <- RunPCA(obj, assay = "SCT", verbose = FALSE)
--- rscripts/cli/spatial_plotting.R
+++ rscripts/cli/spatial_plotting.R
@@ -45,8 +45,8 @@ for (t in c("nCount_Spatial", "nFeature_Spatial", "Malat1", "Neat1")) {
                   adjust = ifelse(!isGene && BIN_SIZE <= 10, 0.9999, 1),
                   custom_colours = colours,
                   left_plot = left_plot,
-                  dpi = 2000,
-                  minres = 1)
+                  dpi = 150,
+                  minres = 150)
   } else {
     # plot vln and spatial plot separately for bin <= 2
     left_plot = VlnPlot(obj, features = t, pt.size = 0.05) + NoLegend()
