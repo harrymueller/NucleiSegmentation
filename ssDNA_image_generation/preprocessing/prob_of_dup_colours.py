3ebf6de017c836cad3708b5a8d18847ecfc8fdcb
Harry <harry@mueller.com.au>
2022-09-23 10:29:21 +0800

    improved ssDNA image generation code

--- ssDNA_image_generation/preprocessing/prob_of_dup_colours.py
+++ ssDNA_image_generation/preprocessing/prob_of_dup_colours.py
@@ -19,8 +19,10 @@ def stirlings_approx_ln(s):
 def gosper_formula(s):
     return 0.5 * ln((2*s + 1/3)*pi) + s*ln(s) - s
 
-#ps = stirlings_approx_ln(n) - stirlings_approx_ln(n - r) - r * ln(n)
+ps = stirlings_approx_ln(n) - stirlings_approx_ln(n - r) - r * ln(n)
 pg = gosper_formula(n) - gosper_formula(n - r) - r * ln(n)
 
+print("Using Stirling's Approximation")
+print(1 - np.exp(ps))
 print("Using Gosper's Formula")
-print(1 - np.power(ld(e),pg))
+print(1 - np.exp(pg))
